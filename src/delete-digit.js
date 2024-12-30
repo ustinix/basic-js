278817f22c3ab4d6d7994383af34f6f4d37601aa
Ustinix <ksuletters@yandex.ru>
2024-12-30 15:55:33 +0300

    count, delete, dream

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const numStr = n.toString();
+  let maxResult = 0;
+
+  for (let i = 0; i < numStr.length; i++) {
+    const modifiedNumStr = numStr.slice(0, i) + numStr.slice(i + 1);
+    const modifiedNum = parseInt(modifiedNumStr, 10);
+    maxResult = Math.max(maxResult, modifiedNum);
+  }
+
+  return maxResult;
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  const letters = members
+  .filter(member => typeof member === 'string')
+  .map(member => member.trim()[0].toUpperCase());
+
+  if (letters.length === 0) {
+    return false;
+  }
+  return letters.sort().join('');
 }
 
 module.exports = {
