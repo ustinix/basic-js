fbe52b4a083b61b6794a50ab20e953e62f2f556e
Ustinix <ksuletters@yandex.ru>
2025-01-06 22:19:08 +0300

    repeat and file and emais

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,22 +2,30 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const repeatTimes = options.repeatTimes || 1;
+  const separator = options.separator || '+';
+  const addition = options.addition !== undefined ? String(options.addition) : '';
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const additionSeparator = options.additionSeparator || '|';
+
+
+  const repeatedAddition = Array(additionRepeatTimes).fill(addition.toString()).join(additionSeparator);
+
+  return Array(repeatTimes).fill(str + repeatedAddition).join(separator);
 }
 
 module.exports = {
--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const list = {};
+
+  return names.map(name => {
+    let newName = name;
+
+    while (list[newName]) {
+      const sufix = `(${list[newName]++})`;
+      newName = name + sufix;
+    }
+
+    list[newName] = 1;
+
+    return newName;
+  });
 }
 
 module.exports = {
